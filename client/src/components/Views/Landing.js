df4376d217951800022f039aab98abe01a170c29
Dshollings <school@Davids-MacBook-Pro.local>
2017-09-27 16:18:16 -0700

    update landing page

--- client/src/components/Views/Landing.js
+++ client/src/components/Views/Landing.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import "./css/bulma.css"
 import "./css/landing.css"
-import "jquery"
+import "jquery" 
 import helpers from "./js/helper.js"
 
 class Landing extends Component {
