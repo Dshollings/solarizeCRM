922a2b831cf386ba3f2cac52c71cc4bc0d9b9cbc
Dshollings <school@Davids-MacBook-Pro.local>
2017-09-29 12:20:09 -0700

    dashboard content

--- server/utils/html-routes.js
+++ server/utils/html-routes.js
@@ -2,15 +2,15 @@ var path = require('path');
 
 module.exports = function(app) {
   app.get("/dashboard", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../client/dashboard/dashboard.html"));
+    res.redirect("https://afternoon-eyrie-66951.herokuapp.com/dashboard.html");
   });
   app.get("/user", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../client/dashboard/user.html"));
+    res.redirect("https://afternoon-eyrie-66951.herokuapp.com/user.html");
   });
   app.get("/table", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../client/dashboard/table.html"));
+    res.redirect("https://afternoon-eyrie-66951.herokuapp.com/table.html");
   });
   app.get("/maps", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../client/dashboard/maps.html"));
+    res.redirect("https://afternoon-eyrie-66951.herokuapp.com/maps.html");
   });
 }
